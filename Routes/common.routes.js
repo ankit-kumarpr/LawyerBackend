cbdf4d61f318cad31fc2e44994fbb27dbc66c225
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-06-21 17:52:13 +0530

    Update common.routes.js

--- Routes/common.routes.js
+++ Routes/common.routes.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken, restrictTo } = require('../middleware/auth');
+const { verifyToken, restrictTo } = require("../middleware/auth");
 
 const {
-    GetAllLawyersList,
-    UpdateAnyLawyerData,
-    DeleteAnyLawyer,
-    ActiveAnyLawyer
-}=require('../Controllers/auth.controller');
+  GetAllLawyersList,
+  UpdateAnyLawyerData,
+  DeleteAnyLawyer,
+  ActiveAnyLawyer,
+  GetAllUserList,
+  UpdateuserData,
+} = require("../Controllers/auth.controller");
 
+router.get("/lwayerlist", GetAllLawyersList);
+router.put("/updatelawyer/:lawyerId", UpdateAnyLawyerData);
+router.put("/dellawyer/:lawyerId", DeleteAnyLawyer); //admin only
+router.put("/activelawyer/:lawyerId", ActiveAnyLawyer); //admin only
 
-router.get('/lwayerlist',GetAllLawyersList);
-router.put('/updatelawyer/:lawyerId',UpdateAnyLawyerData);
-router.post('/dellawyer/:lawyerId',  DeleteAnyLawyer);   //admin only
-router.put('/activelawyer/:lawyerId',  ActiveAnyLawyer);   //admin only
-
-
-
-
+// users
+router.get("/alluser", GetAllUserList); //admin only
+router.put("/updateuser/:userId", UpdateuserData);
 
 module.exports = router;

0eaf8ee8f7d288f170fa44df083333d8a299f305
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-06-21 17:50:05 +0530

    Update Lawyer.Model.js

--- Models/Lawyer.Model.js
+++ Models/Lawyer.Model.js
@@ -31,6 +31,7 @@ const LawyerSchema = new mongoose.Schema({
     required: true,
     minlegth: 8,
   },
+  
  licenseNumber: {
   type: String,
   required: true,
@@ -42,6 +43,21 @@ const LawyerSchema = new mongoose.Schema({
     type:Boolean,
     default:false,
   },
+  status:{
+    type:String,
+    enum:["online","offline","Busy"],
+    default:"offline"
+  },
+  consultation_fees:{
+    type:Number,
+    default:0
+  },
+  role: {
+  type: String,
+  enum: ['lawyer'],
+  default: 'lawyer',
+  required: true
+},
   created_at:{
     type:Date,
     default:Date.now()

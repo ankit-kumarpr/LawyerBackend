eef81d8da6ed4bb7a9326cd483416a4515dd0e66
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-06-21 17:53:11 +0530

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -1,10 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-exports.generateTokens = (user, role) => {
-  const payload = { id: user._id, role };
+exports.generateTokens = (user) => {
+  const payload = {
+    id: user._id,
+    role: user.role
+  };
+  // console.log("payload is",payload);
 
-  const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET, { expiresIn: '15m' });
-  const refreshToken = jwt.sign(payload, process.env.REFRESH_SECRET, { expiresIn: '7d' });
+  const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET, {
+    expiresIn: '15m'
+  });
+
+  const refreshToken = jwt.sign(payload, process.env.REFRESH_SECRET, {
+    expiresIn: '7d'
+  });
 
   return { accessToken, refreshToken };
 };

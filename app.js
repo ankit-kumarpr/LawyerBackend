cb4d6d59967ab22e77993e17cc778cdcddaa6c78
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-06-30 14:04:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Socket.IO setup middleware placeholder
 let io;
 const setSocketIO = (socketInstance) => {

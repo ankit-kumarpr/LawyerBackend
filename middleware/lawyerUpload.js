d6927935a3fdf0ed9fa219e4868045c8ebc3a386
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-07-08 12:32:42 +0530

    Update lawyerUpload.js

--- middleware/lawyerUpload.js
+++ middleware/lawyerUpload.js
@@ -21,6 +21,10 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const lawyerUpload = multer({ storage: lawyerStorage, fileFilter });
+const lawyerUpload = multer({ 
+  storage: lawyerStorage,
+  fileFilter,
+  limits: { fileSize: 10 * 1024 * 1024 } // 10 MB in bytes
+});
 
 module.exports = lawyerUpload;

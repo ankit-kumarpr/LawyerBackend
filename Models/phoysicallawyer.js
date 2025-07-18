886c295d1436b24a1a170598e61236673adf3ab1
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2025-07-18 11:26:38 +0530

    Add files via upload

---
+++ Models/phoysicallawyer.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const PhysicalLawyerSchema = new mongoose.Schema({
+  lawyerId: { type: String, unique: true },
+  name: String,
+  email: String,
+  phone: String,
+  city: String,
+  licenseNumber: String,
+  specialization: String,
+  consultation_fees: Number,
+  experience: Number,
+  practiceArea: String,
+  profileDescription: String,
+  status: { type: String, default: 'online' },
+  isverified: { type: Boolean, default: true },
+  profileImage: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('PhysicalLawyer', PhysicalLawyerSchema);
